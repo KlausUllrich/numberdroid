b3ccc76b389463643184d0a956b9c8a6e5f663d1
KlausUllrich <33103541+KlausUllrich@users.noreply.github.com>
2026-08-16 07:35:50 +0200

    Export geometry compiler stage

--- src/levelgen/index.ts
+++ src/levelgen/index.ts
@@ -1,5 +1,7 @@
 export { compileLevelSpec } from "./compiler";
+export { compileLevelGeometry } from "./geometry";
 export { NUMBERDROID_PROP_REGISTRY } from "./propRegistry";
 export { deriveSubSeed, normalizeLevelSeed, seededUnit } from "./seed";
 export { TS01_LEVEL_SPEC } from "./specs/ts01";
 export type * from "./types";
+export type * from "./geometryTypes";
